0b892755881d2c519cc4beef089d305b9a675a4c
PabloGrzz <pabloentrecomillas@gmail.com>
2025-08-28 16:29:08 +0200

    Arreglo en el formulario de contactsection

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -10,7 +10,7 @@ const ContactSection = () => {
   const initialTextRef = useRef(null);
   const finalTextRef = useRef(null);
 
-  // Estado formulario
+  // Estado para abrir el formulario
   const [contactFormOpen, setContactFormOpen] = useState(false);
   const openContactForm = () => setContactFormOpen(true);
   const closeContactForm = () => setContactFormOpen(false);
@@ -18,18 +18,23 @@ const ContactSection = () => {
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
 
+    // Aseguro matar las instancias de ScrollTrigger
     const cleanup = () => {
       ScrollTrigger.getAll().forEach((st) => {
-        if (st.vars.trigger === sectionRef.current) st.kill();
+        if (st.vars.trigger === sectionRef.current) {
+          st.kill();
+        }
       });
     };
 
     cleanup();
 
+    // Estados iniciales
     gsap.set(circleRef.current, { scale: 1, backgroundColor: "white" });
     gsap.set(initialTextRef.current, { opacity: 1 });
     gsap.set(finalTextRef.current, { opacity: 0 });
 
+    // TimeLine
     const tl = gsap.timeline({
       scrollTrigger: {
         trigger: sectionRef.current,
@@ -44,82 +49,104 @@ const ContactSection = () => {
       },
     });
 
-    tl.to(circleRef.current, {
-      scale: 5,
-      backgroundColor: "#9333EA",
-      ease: "power1.inOut",
-      duration: 0.5,
-    }, 0);
+    // Estado Inicial mid-Zoom
+    tl.to(
+      circleRef.current,
+      {
+        scale: 5,
+        backgroundColor: "#9333EA",
+        ease: "power1.inOut",
+        duration: 0.5,
+      },
+      0
+    );
 
-    tl.to(initialTextRef.current, {
-      opacity: 0,
-      ease: "power1.out",
-      duration: 0.2,
-    }, 0.1);
+    // Borramos texto inicial
+    tl.to(
+      initialTextRef.current,
+      {
+        opacity: 0,
+        ease: "power1.out",
+        duration: 0.2,
+      },
+      0.1
+    );
 
-    tl.to(circleRef.current, {
-      scale: 17,
-      backgroundColor: "#E9D5FF",
-      boxShadow: "0 0 50px 20px rgba(233, 213, 255, 0.3)",
-      ease: "power2.inOut",
-      duration: 0.5,
-    }, 0.5);
+    // mid-Zoom a estado final
+    tl.to(
+      circleRef.current,
+      {
+        scale: 17,
+        backgroundColor: "#E9D5FF",
+        boxShadow: "0 0 50px 20px rgba(233, 213, 255, 0.3)",
+        ease: "power2.inOut",
+        duration: 0.5,
+      },
+      0.5
+    );
 
-    tl.to(finalTextRef.current, {
-      opacity: 1,
-      ease: "power2.in",
-      duration: 0.2,
-    }, 0.7);
+    // Aparece texto final
+    tl.to(
+      finalTextRef.current,
+      {
+        opacity: 1,
+        ease: "power2.in",
+        duration: 0.2,
+      },
+      0.7
+    );
 
     return cleanup;
   }, []);
 
   return (
-    <section
-      ref={sectionRef}
-      className="flex items-center justify-center bg-black relative"
-      style={{ overscrollBehavior: "none" }}
-    >
-      <div
-        ref={circleRef}
-        className="w-24 sm:w-28 md:w-32 h-24 sm:h-28 md:h-32 rounded-full
-        flex items-center justify-center relative transition-shadow duration-1000
-        shadow-violet-300/50 shadow-lg bg-gradient-to-r from-violet-400 to-pink-100"
+    <>
+      <section
+        ref={sectionRef}
+        className="flex items-center justify-center bg-black relative"
+        style={{ overscrollBehavior: "none" }}
       >
-        <p
-          ref={initialTextRef}
-          className="text-black font-bold text-base sm:text-lg md:text-xl absolute inset-0 flex items-center text-center"
-        >
-          DESPLAZA HACIA ABAJO
-        </p>
-
-        {/* Texto final */}
         <div
-          ref={finalTextRef}
-          className="text-center relative flex flex-col items-center justify-center opacity-0"
+          ref={circleRef}
+          className="w-24 sm:w-28 md:w-32 h-24 sm:h-28 md:h-32 rounded-full
+          flex items-center justify-center relative transition-shadow duration-1000
+          shadow-violet-300/50 shadow-lg bg-gradient-to-r from-violet-400 to-pink-100"
         >
-          <h1
-            className="text-black md:w-[15rem] w-full max-w-[35rem] lg:scale-[0.4] sm:scale-[0.25] scale-[0.07] 
-            md:font-bold text-sm sm:text-base leading-none mb-5"
+          <p
+            ref={initialTextRef}
+            className="text-black font-bold text-base sm:text-lg md:text-xl absolute inset-0
+            flex items-center text-center"
           >
-            ¡HOLA! ESTOY BUSCANDO NUEVOS PROYECTOS
-          </h1>
-
-          <p className="text-black lg:w-[40rem] w-[20rem] absolute sm:mt-3 mt-1 md:scale-[0.1] scale-[0.068]">
-            Desarrollador Full Stack especializado en crear aplicaciones modernas, web y móviles. Centrado en clean code y experiencias únicas de usuario. Si tienes un proyecto en mente, estaré encantado de hablar contigo.
+            DESPLAZA HACIA ABAJO
           </p>
 
-          {/* Botón de contacto con formulario */}
-          <button
-            onClick={openContactForm}
-            className="px-10 py-2 rounded-xl bg-black hover:bg-white hover:text-black transition-all duration-500 scale-[0.1] absolute sm:mt-9 mt-7 text-nowrap"
+          {/* Texto final */}
+          <div
+            ref={finalTextRef}
+            className="text-center relative flex flex-col items-center justify-center opacity-0"
           >
-            Contactame
-          </button>
-        </div>
-      </div>
+            <h1
+              className="text-black md:w-[15rem] w-full max-w-[35rem] lg:scale-[0.4] sm:scale-[0.25] scale-[0.07] 
+              md:font-bold text-sm sm:text-base leading-none mb-5"
+            >
+              ¡HOLA! ESTOY BUSCANDO NUEVOS PROYECTOS
+            </h1>
 
-      {/* Formulario modal */}
+            <p className="text-black lg:w-[40rem] w-[20rem] absolute sm:mt-3 mt-1 md:scale-[0.1] scale-[0.068]">
+              Desarrollador Full Stack especializado en crear aplicaciones modernas, web y móviles. Centrado en clean code y experiencias únicas de usuario. Si tienes un proyecto en mente, estaré encantado de hablar contigo.
+            </p>
+
+            <button
+              onClick={openContactForm}
+              className="px-10 py-2 rounded-xl bg-black hover:bg-white hover:text-black transition-all duration-500 scale-[0.1] absolute sm:mt-9 mt-7 text-nowrap"
+            >
+              Contactame
+            </button>
+          </div>
+        </div>
+      </section>
+
+      {/* Formulario fuera de la sección para que no quede fijo */}
       <AnimatePresence>
         {contactFormOpen && (
           <motion.div
@@ -127,39 +154,83 @@ const ContactSection = () => {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.5 }}
-            className="fixed inset-0 bg-black/50 backdrop-blur-sm z-50 flex items-center justify-center p-4"
+            className="fixed inset-0 bg-black/50 background-blur-sm z-50 flex items-center justify-center p-4"
           >
             <motion.div
               initial={{ scale: 0.8, opacity: 0, y: 30 }}
               animate={{ scale: 1, opacity: 1, y: 0 }}
               exit={{ scale: 0.8, opacity: 0, y: 30 }}
-              transition={{ type: "spring", damping: 30, stiffness: 200, duration: 0.8 }}
+              transition={{
+                type: "spring",
+                damping: 30,
+                stiffness: 200,
+                duration: 0.8,
+              }}
               className="bg-white dark:bg-gray-800 rounded-xl shadow-xl w-full max-w-md p-6"
             >
               <div className="flex justify-between items-center mb-4">
                 <h1 className="text-2xl font-bold text-gray-300">Hablemos</h1>
                 <button onClick={closeContactForm}>
-                  <FiX className="w-5 h-5 text-gray-300 font-extrabold" />
+                  <FiX className="w-5 h-5 test-gray-300 font-extrabold" />
                 </button>
               </div>
 
               <form className="space-y-4">
                 <div>
-                  <label htmlFor="name" className="block text-sm font-medium text-gray-300 mb-1">Nombre</label>
-                  <input type="text" id="name" placeholder="Tu nombre" className="w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2 focus:ring-violet-500 focus:border-violet-500 bg-gray-700" />
+                  <label
+                    htmlFor="name"
+                    className="block text-sm font-medium text-gray-300 mb-1"
+                  >
+                    Nombre
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    placeholder="Tu nombre"
+                    className="w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2
+                    focus:ring-violet-500 focus:border-violet-500 bg-gray-700"
+                  />
                 </div>
 
                 <div>
-                  <label htmlFor="email" className="block text-sm font-medium text-gray-300 mb-1">Email</label>
-                  <input type="email" id="email" placeholder="Tu email" className="w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2 focus:ring-violet-500 focus:border-violet-500 bg-gray-700" />
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium text-gray-300 mb-1"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    placeholder="Tu email"
+                    className="w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2
+                    focus:ring-violet-500 focus:border-violet-500 bg-gray-700"
+                  />
                 </div>
 
                 <div>
-                  <label htmlFor="message" className="block text-sm font-medium text-gray-300 mb-1">Mensaje</label>
-                  <textarea rows="4" id="message" placeholder="¿Como podemos ayudarte?" className="w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2 focus:ring-violet-500 focus:border-violet-500 bg-gray-700" />
+                  <label
+                    htmlFor="message"
+                    className="block text-sm font-medium text-gray-300 mb-1"
+                  >
+                    Mensaje
+                  </label>
+                  <textarea
+                    rows="4"
+                    id="message"
+                    placeholder="¿Como podemos ayudarte?"
+                    className="w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2
+                    focus:ring-violet-500 focus:border-violet-500 bg-gray-700"
+                  />
                 </div>
 
-                <motion.button type="submit" whileHover={{ scale: 1.03 }} whileTap={{ scale: 0.97 }} className="w-full px-4 py-2 bg-gradient-to-r from-violet-600 to-violet-400 hover:from-violet-700 hover:to-purple-700 transition-all duration-300 rounded-lg shadow-md hover:shadow-lg hover:shadow-violet-600/50">
+                <motion.button
+                  type="submit"
+                  whileHover={{ scale: 1.03 }}
+                  whileTap={{ scale: 0.97 }}
+                  className="w-full px-4 py-2 bg-gradient-to-r from-violet-600 to-violet-400 hover:from-violet-700 hover:to-purple-700
+                  transition-all duration-300 rounded-lg shadow-md hover:shadow-lg hover:shadow-violet-600/50"
+                >
                   Enviar Mensaje
                 </motion.button>
               </form>
@@ -167,7 +238,7 @@ const ContactSection = () => {
           </motion.div>
         )}
       </AnimatePresence>
-    </section>
+    </>
   );
 };
 
